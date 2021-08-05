d0733869cb201ee42ee3e726409fdf3958efdc20
ntk <43851479+ntk-ta01@users.noreply.github.com>
2021-08-05 12:13:20 +0900

    clear until test 5

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/TechBowl-japan/go-stations/db"
+	"github.com/TechBowl-japan/go-stations/handler"
 )
 
 func main() {
@@ -51,6 +53,8 @@ func realMain() error {
 	mux := http.NewServeMux()
 
 	// TODO: ここから実装を行う
-
+	fmt.Println("server start:")
+	mux.Handle("/healthz", handler.NewHealthzHandler())
+	log.Fatal(http.ListenAndServe(":8080", mux))
 	return nil
 }
